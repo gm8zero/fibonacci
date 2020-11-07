333221f4a9b84e66d07a655980aeff2880aba256
gm8zero <hana87road@icloud.com>
2020-11-08 01:04:29 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use strict';
+const memo = new Map();
+
+memo.set(0,0);  //定義だから最初に決めておく
+memo.set(1,1);
+
 function fib(n){
-    if (n ===0) {
-        return 0;
-    }else if (n===1){
-        return 1;
+    if (memo.has(n)) { //メモにデータがあればそれを使う
+        return memo.get(n);
     }
-    return fib(n-1) + fib(n-2);
+    let value = fib(n-1) + fib(n-2); // メモにデータがないから計算する
+    memo.set(n, value); //計算結果を取っておく
+    return value; //計算結果を返す
 }
-for (let i = 0; i <= 40; i++){
+for (let i = 0; i <= 1000; i++){
     console.log( i + "のフィボナッチは" + fib(i) + "です");
 }
